443324c35cfac723746f64981e4580a0cb314fb1
ferka123 <ferka911@gmail.com>
2022-09-19 21:13:51 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,17 @@ app.use("/api/goals", require("./routes/goalsRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      path.resolve(__dirname, "../frontend/build", "index.html")
     )
   );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
 app.use(errorHandler);
 
